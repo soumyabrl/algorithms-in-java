75ad01ff34f2641a6ab57bfe983471784ecdb32b
soumya <soumyabrl@gmail.com>
2020-11-11 09:51:41 +0530

    Added binary search for ordered array

---
+++ src/binary/search/Main.java
@@ -0,0 +1,46 @@
+package binary.search;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int[] sortedIntArray = new int[100];
+        for(int i = 0 ; i<100; i++){
+            sortedIntArray[i] = i+1;
+        }
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number you want to search within 1 to 100");
+        int numberToSearch = sc.nextInt();
+        binarySearch(sortedIntArray, numberToSearch);
+
+    }
+
+    private static void binarySearch(int[] sortedIntArray, int numberToSearch) {
+        if(numberToSearch <0 || numberToSearch >100){
+            System.out.println("You entered an invalid number. The number should be in the range 1-100");
+            throw new InputMismatchException();
+        }
+        boolean isNumberFound = false;
+        int low = 1;
+        int high = 100;
+        while(!isNumberFound){
+            int mid = (low + high) / 2;
+            if(numberToSearch == mid){
+                System.out.println("Found");
+                isNumberFound = true;
+            }else if(numberToSearch > mid){ // 51 to 100
+                low = mid + 1;
+                System.out.println("Low is " + low);
+            }else{
+                high = mid -1;
+                System.out.println("High is " + high);
+            }
+        }
+
+
+
+    }
+}
--- src/welcome/Welcome.java
+++ src/welcome/Welcome.java
@@ -2,6 +2,6 @@ package welcome;
 
 public class Welcome {
     public static void main(String[] args) {
-        System.out.println("Welcome to algos in Java");
+        System.out.println("Welcome to algorithms in Java - Robert Lafore 2nd Edition");
     }
 }
